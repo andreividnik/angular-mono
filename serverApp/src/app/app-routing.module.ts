636f7c20ea1ae09e5ebccb598f6787d149117208
Andrei Vidnik <andreividnik@gmail.com>
2022-10-29 00:24:02 +0300

    init some projects

---
+++ serverApp/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
